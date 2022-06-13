4cdb9cd6c72a7891ae8c34491612d7e0e3178893
rsousa <rsousa@ciandt.com>
2022-06-13 11:24:55 -0300

    fix: using a literal value of pi and formatting correctelly the string

--- 0x00-java-hello_world/PrintFloatVariable.java
+++ 0x00-java-hello_world/PrintFloatVariable.java
@@ -1,6 +1,6 @@
 class PrintFloatVariable {
     public static void main(String[] args) {
-        float pi = Math.PI;
-        System.out.printf("Valor de pi: %3.f", pi);
+        float pi = 3.14159;
+        System.out.printf("Valor de pi: %.3f\n", pi);
     }
 }
