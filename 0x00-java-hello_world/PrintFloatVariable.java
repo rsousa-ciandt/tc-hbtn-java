01f882193cb614bbb920ff3cb894fd2e2bcbef5b
rsousa <rsousa@ciandt.com>
2022-06-13 11:29:59 -0300

    fix: formatting code to has 6 lines and with a new line

--- 0x00-java-hello_world/PrintFloatVariable.java
+++ 0x00-java-hello_world/PrintFloatVariable.java
@@ -1,6 +1,5 @@
 class PrintFloatVariable {
     public static void main(String[] args) {
-        float pi = 3.14159;
-        System.out.printf("Valor de pi: %.3f\n", pi);
+        float pi = 3.14159f; System.out.printf("Valor de pi: %.3f\n", pi);
     }
 }
