536f680e672081f1830c9c0255aafa97da6ce634
rsousa <rsousa@ciandt.com>
2022-06-13 11:49:01 -0300

    fix: declaration of strings changed, last line added and ! added in string to pass in test

--- 0x00-java-hello_world/PrintStringConcat.java
+++ 0x00-java-hello_world/PrintStringConcat.java
@@ -1,8 +1,8 @@
 class PrintStringConcat {
     public static void main(String[] args) {
-        String texto1 = "Holberton",
-                texto2 = "School",
-                result = String.format("Bem vindo a %s %s", texto1, texto2);
+        String texto1 = "Holberton";
+        String texto2 = "School";
+        String result = String.format("Bem vindo a %s %s!", texto1, texto2);
         System.out.print(result);
     }
 }
