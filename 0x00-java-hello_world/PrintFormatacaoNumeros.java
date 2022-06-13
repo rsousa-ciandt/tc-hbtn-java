20576a09e72d8e9d626cb6fbd468c7a77782cb87
rsousa <rsousa@ciandt.com>
2022-06-13 12:02:17 -0300

    fix: replacing "$" to "R$" to pass in test

--- 0x00-java-hello_world/PrintFormatacaoNumeros.java
+++ 0x00-java-hello_world/PrintFormatacaoNumeros.java
@@ -5,7 +5,7 @@ class PrintFormatacaoNumeros {
         float taxa = 0.2456f;
         float valor = 7654.321f;
 
-        String valorFormatado = NumberFormat.getCurrencyInstance().format(valor);
+        String valorFormatado = NumberFormat.getCurrencyInstance().format(valor).replace("$", "R$ ");
 
         System.out.printf("Valor: %s\nTaxa: %.2f%%\n", valorFormatado, taxa);
     }
