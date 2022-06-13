f56a71a485e900b0d474c0eed6627ade65fb0984
rsousa <rsousa@ciandt.com>
2022-06-13 11:50:10 -0300

    fix: break line added

--- 0x00-java-hello_world/PrintStringConcat.java
+++ 0x00-java-hello_world/PrintStringConcat.java
@@ -2,7 +2,7 @@ class PrintStringConcat {
     public static void main(String[] args) {
         String texto1 = "Holberton";
         String texto2 = "School";
-        String result = String.format("Bem vindo a %s %s!", texto1, texto2);
+        String result = String.format("Bem vindo a %s %s!\n", texto1, texto2);
         System.out.print(result);
     }
 }
