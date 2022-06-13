27311debfe8607914c97e59b5cd9ca83fde62a0b
rsousa <rsousa@ciandt.com>
2022-06-13 11:38:58 -0300

    feat: implementation of task "print string variable"

---
+++ 0x00-java-hello_world/PrintStringVariable.java
@@ -0,0 +1,6 @@
+class PrintStringVariable {
+    public static void main(String[] args) {
+        String texto = "Holberton School";
+        System.out.printf("%s\n%s\n%s\n", texto, texto, texto.substring(0, 9));
+    }
+}
