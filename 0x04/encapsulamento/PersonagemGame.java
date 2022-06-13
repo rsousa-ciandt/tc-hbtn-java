b9f2ca768af0d273770858425dcb4da6315c0501
rsousa <rsousa@ciandt.com>
2022-06-13 16:29:11 -0300

    feat: implmentation of task "encapsulamento"

---
+++ 0x04/encapsulamento/PersonagemGame.java
@@ -0,0 +1,20 @@
+public class PersonagemGame {
+    public String nome;
+    public int saudeAtual;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getSaudeAtual() {
+        return saudeAtual;
+    }
+
+    public void setSaudeAtual(int saudeAtual) {
+        this.saudeAtual = saudeAtual;
+    }
+}
