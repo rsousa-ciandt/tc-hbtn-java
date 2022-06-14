fb700e62010bcccabf505ae9aec24059f5573fd6
rsousa <rsousa@ciandt.com>
2022-06-14 15:21:05 -0300

    feat: implementation of task "Empregado/Endereço"

---
+++ 0x09/agregacao/Empregado.java
@@ -0,0 +1,40 @@
+public class Empregado {
+    private String nome;
+    private int codigo;
+    private Endereco endereco;
+
+    public Empregado(String nome, int codigo, Endereco endereco) {
+        this.nome = nome;
+        this.codigo = codigo;
+        this.endereco = endereco;
+    }
+
+    public void apresentar() {
+        System.out.printf("Codigo: %s\nNome: %s\nBairro: %s\nCidade: %s\nPais: %s",
+                codigo, nome, endereco.getBairro(), endereco.getCidade(), endereco.getPais());
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+}
