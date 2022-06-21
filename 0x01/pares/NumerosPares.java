3a488849156ecb9250d4ee868315edf46cd7a9be
rsousa <rsousa@ciandt.com>
2022-06-21 08:35:56 -0300

    feat: implementation of task "numeros pares 0 a 99"

---
+++ 0x01/pares/NumerosPares.java
@@ -0,0 +1,10 @@
+public class NumerosPares {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            if (i % 2 == 0) {
+                System.out.print(i + (i == 98 ? "" : ", "));
+            }
+        }
+        System.out.println();
+    }
+}
