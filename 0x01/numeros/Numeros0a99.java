6db0c01e2d10c4c102065f63cafbde254d0cbb66
rsousa <rsousa@ciandt.com>
2022-06-21 08:32:49 -0300

    fix: intializing temp variable with 0 instead 1

--- 0x01/numeros/Numeros0a99.java
+++ 0x01/numeros/Numeros0a99.java
@@ -1,6 +1,6 @@
 public class Numeros0a99 {
     public static void main(String[] args) {
-        for (int i = 1; i < 100; i++) {
+        for (int i = 0; i < 100; i++) {
             System.out.print(i + (i == 99 ? "" : ", "));
         }
         System.out.println();
