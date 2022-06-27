e7cecdff798b775f21887e0ad1bd9ae4c03d6d56
rsousa <rsousa@ciandt.com>
2022-06-27 08:14:48 -0300

    feat: implementation of task "resumo pedido"

---
+++ Heranca_Exercicios/resumo_pedido/ItemPedido.java
@@ -0,0 +1,31 @@
+import produtos.Produto;
+
+public class ItemPedido {
+    private int quantidade;
+    private Produto produto;
+
+    public ItemPedido(Produto produto, int quantidade) {
+        setQuantidade(quantidade);
+        setProduto(produto);
+    }
+
+    public double getTotal() {
+        return getProduto().obterPrecoLiquido() * getQuantidade();
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+}
---
+++ Heranca_Exercicios/resumo_pedido/Pedido.java
@@ -0,0 +1,68 @@
+import produtos.Produto;
+
+import java.util.Arrays;
+
+public class Pedido {
+    private double percentualDesconto;
+    private ItemPedido[] itens;
+
+    public Pedido(double percentualDesconto, ItemPedido[] itens) {
+        setPercentualDesconto(percentualDesconto);
+        setItens(itens);
+    }
+
+    public void apresentarResumoPedido() {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        stringBuilder.append("------- RESUMO PEDIDO -------\n");
+
+        for (ItemPedido itemPedido: getItens()) {
+            Produto produto = itemPedido.getProduto();
+            String tipo = produto.getClass().getSimpleName();
+
+            stringBuilder.append(
+                    String.format("Tipo: %s  Titulo: %s  Preco: %.2f  Quant: %d  Total: %.2f\n",
+                            tipo, produto.getTitulo(), produto.obterPrecoLiquido(), itemPedido.getQuantidade(), itemPedido.getTotal())
+            );
+        }
+
+        stringBuilder.append("----------------------------\n");
+        stringBuilder.append(String.format("DESCONTO: %.2f\n", getValorDesconto()));
+        stringBuilder.append(String.format("TOTAL PRODUTOS: %.2f\n", getTotalProdutos()));
+        stringBuilder.append("----------------------------\n");
+        stringBuilder.append(String.format("TOTAL PEDIDO: %.2f\n", calcularTotal()));
+
+        System.out.println(stringBuilder.toString());
+    }
+
+    public double getTotalProdutos() {
+        return Arrays
+                .stream(getItens())
+                .mapToDouble(ItemPedido::getTotal)
+                .sum();
+    }
+
+    public double getValorDesconto() {
+        return getTotalProdutos() * getPercentualDesconto() / 100;
+    }
+
+    public double calcularTotal() {
+        return getTotalProdutos() / getValorDesconto();
+    }
+
+    public void setPercentualDesconto(double percentualDesconto) {
+        this.percentualDesconto = percentualDesconto;
+    }
+
+    public void setItens(ItemPedido[] itens) {
+        this.itens = itens;
+    }
+
+    public double getPercentualDesconto() {
+        return percentualDesconto;
+    }
+
+    public ItemPedido[] getItens() {
+        return itens;
+    }
+}
