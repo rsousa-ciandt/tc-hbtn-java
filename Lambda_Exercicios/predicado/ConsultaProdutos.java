f16b1800c096dbb4e6b6b6b5d14cccf141b59795
rsousa <rsousa@ciandt.com>
2022-07-11 07:23:07 -0300

    feat: implementation of taks "consultar produtos com predicado"

---
+++ Lambda_Exercicios/predicado/ConsultaProdutos.java
@@ -0,0 +1,9 @@
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class ConsultaProdutos {
+    public static List<Produto> filtrar(List<Produto> produtos, Predicate<Produto> filtro) {
+        return produtos.stream().filter(filtro).collect(Collectors.toList());
+    }
+}
