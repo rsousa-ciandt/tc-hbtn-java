3a5133052604dd1a4239b7a2baad5905f6be1980
rsousa <rsousa@ciandt.com>
2022-07-11 15:42:55 -0300

    feat: implementatio of task "pessoas agrupadas por cargos reverse"

---
+++ Streams_Collector_Optional_Exercicios/pessoas_por_cargo_ordem_reversa/ConsultaPessoas.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class ConsultaPessoas {
+    public static TreeMap<String, TreeSet<Pessoa>> obterPessoasAgrupadasPorCargoEmOrdemReversa(List<Pessoa> pessoas) {
+        TreeMap<String, TreeSet<Pessoa>> treeMap = pessoas.stream()
+                .collect(
+                        Collectors.groupingBy(
+                                Pessoa::getCargo,
+                                () -> new TreeMap<>(Comparator.reverseOrder()),
+                                Collectors.toCollection(TreeSet::new)
+                        )
+                );
+
+        return treeMap;
+    }
+}
