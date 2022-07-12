0923a317b62f557dc734b3e4dd60b881fc9925ab
rsousa <rsousa@ciandt.com>
2022-07-12 07:59:35 -0300

    feat: implementation of task "hobbies por cargo"

---
+++ Streams_Collector_Optional_Exercicios/hobbies_por_cargo/ConsultaPessoas.java
@@ -0,0 +1,66 @@
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class ConsultaPessoas {
+    private static Function<Pessoa, String> extrairCargoPessoa = (Pessoa::getCargo);
+
+    public static Map<String, TreeSet<String>> obterHobbiesPorCargo(List<Pessoa> pessoas) {
+        return pessoas.stream()
+                .collect(
+                        Collectors.groupingBy(
+                            extrairCargoPessoa,
+                            Collectors.flatMapping(
+                                    p -> p.getHobbies().stream(),
+                                    Collectors.toCollection(TreeSet::new)
+                            )
+                        )
+                );
+    }
+
+    public static Map<String, Double> obterMediaSalarioPorCargo(List<Pessoa> pessoas) {
+        return pessoas.stream()
+                .collect(
+                        Collectors.groupingBy(
+                                extrairCargoPessoa,
+                                Collectors.averagingDouble(Pessoa::getSalario)
+                        )
+                );
+    }
+
+    public static Map<String, Map<Integer, Long>> obterContagemPessoasPorCargoEIdade(List<Pessoa> pessoas) {
+        return pessoas.stream()
+                .collect(
+                        Collectors.groupingBy(
+                                extrairCargoPessoa,
+                                Collectors.groupingBy(
+                                        Pessoa::getIdade,
+                                        Collectors.counting()
+                                )
+                        )
+                );
+    }
+
+    public static Map<String, Long> obterContagemPessoasPorCargo(List<Pessoa> pessoas) {
+        return pessoas.stream()
+                .collect(
+                        Collectors.groupingBy(
+                                extrairCargoPessoa,
+                                Collectors.counting()
+                        )
+                );
+    }
+
+    public static TreeMap<String, TreeSet<Pessoa>> obterPessoasAgrupadasPorCargoEmOrdemReversa(List<Pessoa> pessoas) {
+        TreeMap<String, TreeSet<Pessoa>> treeMap = pessoas.stream()
+                .collect(
+                        Collectors.groupingBy(
+                                extrairCargoPessoa,
+                                () -> new TreeMap<>(Comparator.reverseOrder()),
+                                Collectors.toCollection(TreeSet::new)
+                        )
+                );
+
+        return treeMap;
+    }
+}
