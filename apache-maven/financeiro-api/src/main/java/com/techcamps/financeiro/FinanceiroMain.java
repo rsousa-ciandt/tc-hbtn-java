8425c11528541d4a29ae42583de3c63c70d09303
rsousa <rsousa@ciandt.com>
2022-07-15 09:40:20 -0300

    feat: implementation of apache maven task

---
+++ apache-maven/financeiro-api/src/main/java/com/techcamps/financeiro/FinanceiroMain.java
@@ -0,0 +1,13 @@
+package com.techcamps.financeiro;
+
+/**
+ * Hello world!
+ *
+ */
+public class FinanceiroMain
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Criando um projeto Java Maven" );
+    }
+}
